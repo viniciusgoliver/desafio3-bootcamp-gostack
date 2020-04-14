5ecc6eae2179279383e4593fb09413a32eeeca27
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2020-04-13 21:03:43 -0300

    Adicionando Handles de add e del

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Desafio 3 - Gostack',
+      url: 'https://github.com/viniciusgoliver/desafio3-bootcamp-gostack',
+      techs: ['Node.js', 'ReasctJS'],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const repositoriesFilter = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(repositoriesFilter);
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
